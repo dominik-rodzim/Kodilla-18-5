31e2bb94d61e0c03485b1f566df5d4e9d5710578
dominik-rodzim <46093484+dominik-rodzim@users.noreply.github.com>
2019-03-20 20:10:43 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -6,8 +6,8 @@ var Contact = React.createClass({
     return <div className={'contactItem'}>
       <img className={'contactImage'} src={'http://icons.veryicon.com/256/System/Dynamic%20Yosemite/Contacts.png'}/>
       <div className={'label-wrapper'}>
-        <p className={'contactLabel'}>Imię: {this.props.contact.firstName}</p>
-        <p className={'contactLabel'}>Nazwisko: {this.props.contact.lastName}</p>
+        <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
+        <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
         <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>{this.props.item.email}</a>
       </div>
     </div>
